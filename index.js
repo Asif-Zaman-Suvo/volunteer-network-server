88664f997c1de11ecc277a7ad46af47d7493fb8f
ShuvoCSE176 <asifshuvo119181@gmail.com>
2020-10-05 20:00:22 +0600

    not satisfied today

--- index.js
+++ index.js
@@ -16,20 +16,37 @@ const port = 5000;
 
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("volunteerNetwork").collection("information");
+  const infoCollection = client.db("volunteerNetwork").collection("information");
   console.log("database connected")
   
 
   app.post('/addInfo',(req,res)=>{
     const newRegister =req.body;
-    console.log(newRegister)
+    infoCollection.insertOne(newRegister)
+    .then(result=>{
+      res.send(result.insertedCount>0)
+    })
+   
 
   })
 
+  app.get('/info',(req,res)=>{
+    infoCollection.find({email:req.query.email})
+    .toArray((err,documents)=>{
+      res.send(documents)
 
+    })
 
-  
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    infoCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
 });
+
 
 app.get('/', (req, res) => {
   res.send('this is working')
